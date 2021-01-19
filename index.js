f5ceb8fcab18e81c7c760298e1f19aedf45cc5ca
Anish <sunaranish17@gmail.com>
2021-01-19 13:37:25 +0545

    resolvers added

--- index.js
+++ index.js
@@ -3,23 +3,9 @@ const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
-const Post = require('./models/Post');
+const resolvers = require('./graphql/resolvers'); 
 const { MONGODB } = require('./config.js');
 
-
-
-const resolvers = {
-    Query: {
-       async getPosts() {
-        try {
-            const posts = await Post.find();
-            return posts;
-        }catch (err) {
-            throw new Error(err);
-        }
-       }
-    }
-}
 
 const server = new ApolloServer({
     typeDefs,
