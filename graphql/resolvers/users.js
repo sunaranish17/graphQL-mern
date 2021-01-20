70d07a67949157ddc9669e0e5444e9fa49a49f12
Anish <sunaranish17@gmail.com>
2021-01-20 20:50:26 +0545

    Logic to save a new user

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,4 +1,6 @@
 const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); 
 
 
 module.exports = {
@@ -6,6 +8,19 @@ module.exports = {
         register(_, { registerInput: { username, email, password, confirmPassword } }, context, info) {
             //TODO validate user data
             //TODO Make sure user doesnot already exist
+            //TODO hash password and create an auth token
+            password = await bcrypt.hash(password, 12);
+
+            const newUser = new User({
+                email,
+                username, 
+                password, 
+                createdAt: new Date().toISOString()
+            });
+
+            const res = await newUser.save();
+
+            
         }
     }
 }
