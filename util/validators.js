2686ac49e5a56c894a3db0ead42132975f6f24e9
Anish <sunaranish17@gmail.com>
2021-01-21 21:28:12 +0545

    WIP validators

---
+++ util/validators.js
@@ -0,0 +1,16 @@
+module.exports.validateRegisterInput = (
+    username,
+    email,
+    password,
+    confirmPassword
+) => {
+    const errors = {};
+    if (username.trim() === '') {
+        errors.username = 'Username must not be empty';
+    }
+    if (email.trim() === '') {
+        errors.email = 'email must not be empty';
+    } else {
+        const regEx = aaa
+    }
+}
