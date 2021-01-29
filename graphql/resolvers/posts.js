1f5e481d1c2e5daf872da70ecdf5ff7b8848fe32
Anish <sunaranish17@gmail.com>
2021-01-29 19:44:00 +0545

    Create post api created

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,10 +1,11 @@
 const Post = require('../../models/Post');
+const checkAuth = require('../../util/checkAuth')
 
 module.exports = {
     Query: {
         async getPosts() {
             try {
-                const posts = await Post.find();
+                const posts = await Post.find().sort({ createdAt: -1 });
                 return posts;
             } catch (err) {
                 throw new Error(err);
@@ -25,7 +26,19 @@ module.exports = {
     },
     Mutation: {
         async createPost(_, { body }, context) {
-            
+            const user = checkAuth(context);
+            console.log("user>>>", user)
+
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            });
+
+            const post = await newPost.save();
+
+            return post;
         }
     }
 };
