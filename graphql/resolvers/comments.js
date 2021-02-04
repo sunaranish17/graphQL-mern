a0d6f57bb4cd38699479fa13ef996413a6d46756
Anish <sunaranish17@gmail.com>
2021-02-04 20:06:18 +0545

    Fixed: issue of username not being found

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -28,14 +28,14 @@ module.exports = {
                 return post;
             } else throw new UserInputError('Post not found');
         },
-        async deleteComment(_, {postId, commentId}, context) {
-            const {username} = checkAuth(context);
+        async deleteComment(_, { postId, commentId }, context) {
+            const { username } = checkAuth(context);
+            console.log("This is username>>", username);
 
             const post = await Post.findById(postId);
 
             if (post) {
                 const commentIndex = post.comments.findIndex(c => c.id === commentId);
-
                 if (post.comments[commentIndex].username === username) {
                     post.comments.splice(commentIndex, 1);
                     await post.save();
