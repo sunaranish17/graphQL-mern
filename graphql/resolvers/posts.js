016feb7dfd1551a26b008c15fe6bcbf566566a82
Anish <sunaranish17@gmail.com>
2021-02-05 21:25:37 +0545

    Like feature implemented

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/checkAuth')
 
@@ -41,7 +41,7 @@ module.exports = {
 
             return post;
         },
-        async deletePost(_, {postId}, context) {
+        async deletePost(_, { postId }, context) {
             const user = checkAuth(context);
 
             try {
@@ -56,8 +56,25 @@ module.exports = {
                 throw new Error(error);
             }
         },
-        async likePost(_, {postId}, context) {
-            
+        async likePost(_, { postId }, context) {
+            const { username } = context;
+
+            const post = await Post.findById(postId);
+            if (post) {
+                if (post.likes.find(like => like.username === username)) {
+                    //Post already liked, unlike it
+                    post.likes = post.likes.filter(like => like.username !== username);
+                } else {
+                    //Not liked, like it
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    })
+                }
+
+                await post.save();
+                return post;
+            } else throw new UserInputError('Post not found');
         }
     }
 };
