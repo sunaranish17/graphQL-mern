33995e68e7ae276607a6f0e3b43298248b0ca45f
Anish <sunaranish17@gmail.com>
2021-02-08 12:30:24 +0545

    Minor update on server

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
 const { AuthenticationError, UserInputError } = require('apollo-server');
+const { argsToArgsConfig } = require('graphql/type/definition');
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/checkAuth')
 
@@ -29,6 +30,10 @@ module.exports = {
         async createPost(_, { body }, context) {
             const user = checkAuth(context);
             console.log("user>>>", user)
+
+            if (args.body.trim() === '') {
+                throw new Error('Post body must not be empty');
+            }
 
             const newPost = new Post({
                 body,
