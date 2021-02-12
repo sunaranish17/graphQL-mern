3e2d45bbe2c217dfca75e368341e528fa463d1f4
Anish <sunaranish17@gmail.com>
2021-02-12 19:31:14 +0545

    Import route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import Home from './pages/home';
 import Login from './pages/login';
