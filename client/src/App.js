8256fff11634a890844ee93ddac583033ebb121f
Anish <sunaranish17@gmail.com>
2021-02-15 22:08:29 +0545

    Container added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react';
 import Home from './pages/home';
 import Login from './pages/login';
 import Register from './pages/register';
@@ -10,10 +11,12 @@ import MenuBar from './components/MenuBar';
 function App() {
   return (
     <Router>
-      <MenuBar />
-      <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/register' component={Register} />
+      <Container>
+        <MenuBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Container>
     </Router>
   );
 }
