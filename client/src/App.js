310ac059125298330298ba1fd4b62e4e97340eb5
Anish <sunaranish17@gmail.com>
2021-02-15 21:10:02 +0545

    Menu bar on App

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,12 @@ import 'semantic-ui-css/semantic.min.css';
 import Home from './pages/home';
 import Login from './pages/login';
 import Register from './pages/register';
+import MenuBar from './components/MenuBar';
 
 function App() {
   return (
     <Router>
+      <MenuBar />
       <Route exact path='/' component={Home} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/register' component={Register} />
--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,6 +1,5 @@
-import { set } from 'mongoose';
 import React, { useState } from 'react'
-import { Menu, Segment } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react'
 
 function MenuBar() {
     const [activeItem, setActiveItem] = useState('');
@@ -9,33 +8,26 @@ function MenuBar() {
 
 
     return (
-        <div>
-            <Menu pointing secondary>
-                <Menu.Item
-                    name='home'
-                    active={activeItem === 'home'}
-                    onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                    name='messages'
-                    active={activeItem === 'messages'}
-                    onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                    name='friends'
-                    active={activeItem === 'friends'}
-                    onClick={this.handleItemClick}
-                />
-                <Menu.Menu position='right'>
-                    <Menu.Item
-                        name='logout'
-                        active={activeItem === 'logout'}
-                        onClick={this.handleItemClick}
-                    />
-                </Menu.Menu>
-            </Menu>
+        <Menu pointing secondary>
+            <Menu.Item
+                name='home'
+                active={activeItem === 'home'}
+                onClick={handleItemClick}
+            />
 
-        </div>
+            <Menu.Menu position='right'>
+                <Menu.Item
+                    name='login'
+                    active={activeItem === 'login'}
+                    onClick={handleItemClick}
+                />
+                <Menu.Item
+                    name='logout'
+                    active={activeItem === 'logout'}
+                    onClick={handleItemClick}
+                />
+            </Menu.Menu>
+        </Menu>
     )
 }
 
