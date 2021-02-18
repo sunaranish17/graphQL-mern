2359e372004fe0342b08e7f7d33a467093f0a3c3
Anish <sunaranish17@gmail.com>
2021-02-18 21:18:47 +0545

    WIP post card

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Card, Icon, Label } from 'semantic-ui-react'
+import { Card, Icon, Label, Image } from 'semantic-ui-react'
 
 function PostCard({ post: { body, createdAt, id, username, likeCount, commentCount, likes } }) {
     return (
-        <div>
-            
-        </div>
+        <Card>
+            <Card.Content>
+                <Image floated="right" size="mini" src="" />
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{username}</Card.Meta>
+            </Card.Content>
+        </Card>
     )
 }
 
