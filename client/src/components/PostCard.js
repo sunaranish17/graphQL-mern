062a30be7f36d06d4cad79a15ba2c4006534e353
Anish <sunaranish17@gmail.com>
2021-02-22 23:30:22 +0545

    Link on time and date

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Card, Icon, Label, Image, Button } from 'semantic-ui-react'
+import {Link} from 'react-router-dom';
 import moment from 'moment';
 
 function PostCard({
     post: { body, createdAt, id, username, likeCount, commentCount, likes }
 }) {
     return (
-        <Card>
+        <Card fluid>
             <Card.Content>
                 <Image floated="right" size="mini" src="https://react.semantic-ui.com/images/avatar/large/molly.png" />
                 <Card.Header>{username}</Card.Header>
-                <Card.Meta>{moment(createdAt).formatNow()}</Card.Meta>
+                <Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).formatNow()}</Card.Meta>
                 <Card.Description>
                     {body}
                 </Card.Description>
