fb16198cf9b87ea01f111fdf1d95603044ac0f61
Anish <sunaranish17@gmail.com>
2021-02-23 22:08:47 +0545

    Button for like added

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card, Icon, Label, Image, Button } from 'semantic-ui-react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
 
 function PostCard({
@@ -17,7 +17,14 @@ function PostCard({
                 </Card.Description>
             </Card.Content>
             <Card.Content extra>
-
+                <Button as='div' labelPosition='right'>
+                    <Button color='teal'>
+                        <Icon name='heart' />
+                    </Button>
+                    <Label basic color='teal' pointing='left'>
+                       {likeCount}
+                    </Label>
+                </Button>
             </Card.Content>
         </Card>
     )
