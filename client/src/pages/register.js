22a3ad2798b3a536435e2c7bb4deece9ea9aa003
Anish <sunaranish17@gmail.com>
2021-02-24 22:16:01 +0545

    Register UI

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Form } from 'semantic-ui-react'
 
 function Register() {
+
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
     return (
         <div>
-            <h1>Register page</h1>
+            <Form onSubmit={ } noValidate>
+                <h1>Register</h1>
+                <Form.Input
+                    label="Username"
+                    placeholder="Username..."
+                    name="username"
+                    value={values.username}
+                    onChange={ }
+                />
+                <Form.Input
+                    label="Email"
+                    placeholder="Email..."
+                    name="email"
+                    value={values.email}
+                    onChange={ }
+                />
+                <Form.Input
+                    label="Password"
+                    placeholder="Password..."
+                    name="password"
+                    value={values.password}
+                    onChange={ }
+                />
+                <Form.Input
+                    label="Confirm Password"
+                    placeholder="Confirm Password..."
+                    name="confirmPassword"
+                    value={values.confirmPassword}
+                    onChange={ }
+                />
+
+                <Button type="submit" primary>Register</Button>
+            </Form>
         </div>
     )
 }
