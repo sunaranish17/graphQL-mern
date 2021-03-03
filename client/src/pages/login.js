96c39d5dd34851b78105bc9db3c65127cb64ad97
Anish <sunaranish17@gmail.com>
2021-03-03 11:10:13 +0545

    AUth context on login

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Button, Form } from 'semantic-ui-react'
 import gql from 'graphql-tag'
 import { useMutation } from '@apollo/react-hooks';
 import { useForm } from '../util/hooks';
+import {AuthContext} from '../context/auth';
 
 function Login(props) {
+    const context = useContext(AuthContext);
     const [errors, setErrors] = useState({});
 
     const { onChange, onSubmit, values } = useForm(loginUserCallback, {
@@ -13,8 +15,9 @@ function Login(props) {
     });
 
     const [loginUser, { loading }] = useMutation(LOGIN_USER, {
-        update(_, result) {
-            console.log(result);
+        update(_, {data: {login: userData}}) {
+            // console.log(result.data.login);
+            context.login(userData);
             props.history.push('/');
         },
         onError(err) {
