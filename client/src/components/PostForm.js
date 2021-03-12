819641dda061a30e2ae4f7d89e220e61c9e559bf
Anish <sunaranish17@gmail.com>
2021-03-12 10:31:16 +0545

    WIP post form

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Button, Form } from 'semantic-ui-react'
+
+import { useForm } from '../util/hooks'
 
 function PostForm() {
+
+    const onSubmit = () => {
+
+    };
+
     return (
-        <div>
-            
-        </div>
+        <Form onSubmit={onSubmit}>
+            <h2>Create a post:</h2>
+            <Form.Field>
+                <Form.Input
+                    placeholder="Hi World"
+                    name="body"
+                    onChange={onChange}
+                    value={values.body}
+                />
+                <Button type="submit" color="teal">
+                    Submit
+                </Button>
+            </Form.Field>
+        </Form>
     )
 }
 
