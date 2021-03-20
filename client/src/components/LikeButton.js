545c5f65ea75fdb587a2684f9673c096c7e4d34a
Anish <sunaranish17@gmail.com>
2021-03-20 19:58:13 +0545

    WIP like button

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -7,14 +7,32 @@ function LikeButton({ post: { id, likeCount, likes } }) {
     const [liked, setLiked] = useState(false);
 
     useEffect(() => {
+        if (user && likes.find(like => like.username === user.username)) {
+            setLiked(true);
+        } else {
+            setLiked(false);
+        }
+    }, [user, likes]);
 
-    }, []);
-    
-    return (
-        <Button as='div' labelPosition='right' onClick={likePost}>
+    const likeButton = user ? (
+        liked ? (
+            <Button color='teal'>
+                <Icon name='heart' />
+            </Button>
+        ) : (
             <Button color='teal' basic>
                 <Icon name='heart' />
             </Button>
+        )
+    ) : (
+        <Button as={Link} to="/login" color='teal' basic>
+            <Icon name='heart' />
+        </Button>
+    )
+
+    return (
+        <Button as='div' labelPosition='right' onClick={likePost}>
+            {likeButton}
             <Label basic color='teal' pointing='left'>
                 {likeCount}
             </Label>
