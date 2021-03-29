d3437a24e0e5b0ef0f9a9f3d1c4fd3410a2fa53e
Anish <sunaranish17@gmail.com>
2021-03-29 22:01:54 +0545

    gql query for single post

--- client/src/pages/singlePost.js
+++ client/src/pages/singlePost.js
@@ -1,11 +1,29 @@
+import { gql } from 'graphql-tag';
 import React from 'react'
 
-function SinglePost() {
+function SinglePost(props) {
+    const postId = props.match.params.postId;
     return (
         <div>
-            
+
         </div>
     )
 }
+
+const FETCH_POST_QUERY = gql`
+    query($postId: ID!) {
+        getPost(postId: $postId) {
+            id body createdAt username likeCount
+        }
+        likes{
+            username
+        }
+        likeCount
+        commentCount
+        comments{
+            id username createdAt body
+        }
+    }
+`;
 
 export default SinglePost
